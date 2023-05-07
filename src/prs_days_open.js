3cbf1755dd638f2f2ed5921b11f2dd4a472517b6
apiraino <apiraino@users.noreply.github.com>
2023-05-07 20:25:26 +0200

    refactor PRs js

--- src/prs_days_open.js
+++ src/prs_days_open.js
@@ -9,206 +9,270 @@ import data_prs_days_open_2020 from '../2020.json';
 
 (async function() {
 
-  const opts = {
-    scales: {
-      x: {
-        //stacked:true,
-        title: {display:true, text:'Week of the year', font:{size:24}}
-      },
-      y: {
-        //stacked:true,
-        title: {display:true, text:'Num PRs', font:{size:24}}
-      },
+    const zoomOptions = {
+    pan: {
+      enabled:true,
+      mode: 'xy'
     },
-    elements: {
-      line: {
-        tension: 0.4,
-        fill: true
-      }
+    limits: {
+      x: {min: 0, max: 'original'},
+      y: {min: 0, max: 'original'},
     },
-    plugins: {
-      zoom: {
-        pan: {
-          enabled:true,
-          mode: 'xy'
-        },
-        limits: {
-          x: {min: 0, max: 'original'},
-          y: {min: 0, max: 'original'},
-        },
-        // dynamically enable the zoom
-        // https://github.com/chartjs/chartjs-plugin-zoom/blob/master/docs/samples/wheel/click-zoom.md
-        zoom: {
-          wheel: {
-            enabled:true
-          },
-          pinch: {
-            enabled:true
-          },
-          mode: 'xy'
-        },
+    // zoom is enabled by event onClick
+    zoom: {
+      wheel: {
+        enabled: false
       },
-      legend:{
-        labels:{
-          font: {
-            size:24
-          }
-        },
+      pinch: {
+        enabled: false
       },
-      title: {
-        display:true,
+      mode: 'xy'
+    },
+  };
+  const zoomStatus = () => 'Zoom: ' + (zoomOptions.zoom.wheel.enabled ? 'enabled' : 'disabled');
+
+  const scales = {
+    x: {
+      title: {display:true, text:'Week of the year', font:{size:24}}
+    },
+    y: {
+      title: {display:true, text:'Num PRs', font:{size:24}}
+    },
+  };
+
+  const plugins = {
+    zoom: zoomOptions,
+    legend: {
+      labels: {
         font: {
           size:24
-        },
-        text: 'Change me ...'
-      }
+        }
+      },
+    },
+    title: {
+      display:true,
+      font: {
+        size:24
+      },
+      text: 'Change me ...',
     }
-  }
+  };
 
-  // clone config and change title for each graph
-  var opts_2023 = structuredClone(opts);
-  var opts_2022 = structuredClone(opts);
-  var opts_2021 = structuredClone(opts);
-  var opts_2020 = structuredClone(opts);
-  opts_2023.plugins.title.text='[2023] How long are PRs sitting open before being closed?'
-  opts_2022.plugins.title.text='[2022] How long are PRs sitting open before being closed?'
-  opts_2021.plugins.title.text='[2021] How long are PRs sitting open before being closed?'
-  opts_2020.plugins.title.text='[2020] How long are PRs sitting open before being closed?'
+  const elements = {
+    line: {
+      tension: 0.4,
+      fill:true
+    }
+  };
+
+  // end of common configs
 
   const ctx_prs_days_open_2023 = document.getElementById('prs_days_open_2023');
   const ctx_prs_days_open_2022 = document.getElementById('prs_days_open_2022');
   const ctx_prs_days_open_2021 = document.getElementById('prs_days_open_2021');
   const ctx_prs_days_open_2020 = document.getElementById('prs_days_open_2020');
 
-  // PRs closed in 2023
+  // PRs 2023: configure and create
+  const data_2023 = {
+    labels: data_prs_days_open_2023.map(row => row.woy),
+    datasets: [
+      {
+        label: 'Up to 10 days',
+        data: data_prs_days_open_2023.map(row => row.up_to_10),
+        backgroundColor: '#66b237'
+      },
+      {
+        label: 'Up to 30 days',
+        data: data_prs_days_open_2023.map(row => row.up_to_30),
+        backgroundColor: 'orange'
+      },
+      {
+        label: 'More than 60 days',
+        data: data_prs_days_open_2023.map(row => row.over_60),
+        backgroundColor: 'red'
+      }
+    ]
+  };
+
+  // clone plugin config and customize title
+  const plugins_2023 = structuredClone(plugins);
+  plugins_2023.title.text='[2023] Contrast PRs and bugfixes'
+  const config_2023 = {
+    type: 'line',
+    data: data_2023,
+    options: {
+      scales: scales,
+      plugins: plugins_2023,
+      elements: elements,
+      onClick(e) {
+        const chart = e.chart;
+        console.debug('Clicked: ' + chart.options.plugins.title.text)
+        chart.options.plugins.zoom.zoom.wheel.enabled = !chart.options.plugins.zoom.zoom.wheel.enabled;
+        chart.options.plugins.zoom.zoom.pinch.enabled = !chart.options.plugins.zoom.zoom.pinch.enabled;
+        chart.update();
+      },
+    },
+  };
+
   const chart_2023 = new Chart(
     ctx_prs_days_open_2023,
-    {
-      type: 'line',
-      options: opts_2023,
-      data: {
-        labels: data_prs_days_open_2023.map(row => row.woy),
-        datasets: [
-          {
-            label: 'Up to 10 days',
-            data: data_prs_days_open_2023.map(row => row.up_to_10),
-            backgroundColor: '#66b237'
-          },
-          {
-            label: 'Up to 30 days',
-            data: data_prs_days_open_2023.map(row => row.up_to_30),
-            backgroundColor: 'orange'
-          },
-          {
-            label: 'More than 60 days',
-            data: data_prs_days_open_2023.map(row => row.over_60),
-            backgroundColor: 'red'
-          }
-        ]
-      },
-    }
+    config_2023
   );
 
-  // PRs closed in 2022
+  // PRs 2022: configure and create
+  const data_2022 = {
+    labels: data_prs_days_open_2022.map(row => row.woy),
+    datasets: [
+      {
+        label: 'Up to 10 days',
+        data: data_prs_days_open_2022.map(row => row.up_to_10),
+        backgroundColor: '#66b237'
+      },
+      {
+        label: 'Up to 30 days',
+        data: data_prs_days_open_2022.map(row => row.up_to_30),
+        backgroundColor: 'orange'
+      },
+      {
+        label: 'More than 60 days',
+        data: data_prs_days_open_2022.map(row => row.over_60),
+        backgroundColor: 'red'
+      }
+    ]
+  };
+
+  // clone plugin config and customize title
+  const plugins_2022 = structuredClone(plugins);
+  plugins_2022.title.text='[2022] Contrast PRs and bugfixes'
+  const config_2022 = {
+    type: 'line',
+    data: data_2022,
+    options: {
+      scales: scales,
+      plugins: plugins_2022,
+      elements: elements,
+      onClick(e) {
+        const chart = e.chart;
+        console.debug('Clicked: ' + chart.options.plugins.title.text)
+        chart.options.plugins.zoom.zoom.wheel.enabled = !chart.options.plugins.zoom.zoom.wheel.enabled;
+        chart.options.plugins.zoom.zoom.pinch.enabled = !chart.options.plugins.zoom.zoom.pinch.enabled;
+        chart.update();
+      },
+    },
+  };
+
   const chart_2022 = new Chart(
     ctx_prs_days_open_2022,
-    {
-      type: 'line',
-      options: opts_2022,
-      data: {
-        labels: data_prs_days_open_2022.map(row => row.woy),
-        datasets: [
-          {
-            label: 'Up to 10 days',
-            data: data_prs_days_open_2022.map(row => row.up_to_10),
-            backgroundColor: '#66b237'
-          },
-          {
-            label: 'Up to 30 days',
-            data: data_prs_days_open_2022.map(row => row.up_to_30),
-            backgroundColor: 'orange'
-          },
-          {
-            label: 'More than 60 days',
-            data: data_prs_days_open_2022.map(row => row.over_60),
-            backgroundColor: 'red'
-          }
-        ]
-      },
-    }
+    config_2022
   );
 
-  // PRs closed in 2021
+  // PRs 2021: configure and create
+  const data_2021 = {
+    labels: data_prs_days_open_2021.map(row => row.woy),
+    datasets: [
+      {
+        label: 'Up to 10 days',
+        data: data_prs_days_open_2021.map(row => row.up_to_10),
+        backgroundColor: '#66b237'
+      },
+      {
+        label: 'Up to 30 days',
+        data: data_prs_days_open_2021.map(row => row.up_to_30),
+        backgroundColor: 'orange'
+      },
+      {
+        label: 'More than 60 days',
+        data: data_prs_days_open_2021.map(row => row.over_60),
+        backgroundColor: 'red'
+      }
+    ]
+  };
+
+  // clone plugin config and customize title
+  const plugins_2021 = structuredClone(plugins);
+  plugins_2021.title.text='[2021] Contrast PRs and bugfixes'
+  const config_2021 = {
+    type: 'line',
+    data: data_2021,
+    options: {
+      scales: scales,
+      plugins: plugins_2021,
+      elements: elements,
+      onClick(e) {
+        const chart = e.chart;
+        console.debug('Clicked: ' + chart.options.plugins.title.text)
+        chart.options.plugins.zoom.zoom.wheel.enabled = !chart.options.plugins.zoom.zoom.wheel.enabled;
+        chart.options.plugins.zoom.zoom.pinch.enabled = !chart.options.plugins.zoom.zoom.pinch.enabled;
+        chart.update();
+      },
+    },
+  };
+
   const chart_2021 = new Chart(
     ctx_prs_days_open_2021,
-    {
-      type: 'line',
-      options: opts_2021,
-      data: {
-        labels: data_prs_days_open_2021.map(row => row.woy),
-        datasets: [
-          {
-            label: 'Up to 10 days',
-            data: data_prs_days_open_2021.map(row => row.up_to_10),
-            backgroundColor: '#66b237'
-          },
-          {
-            label: 'Up to 30 days',
-            data: data_prs_days_open_2021.map(row => row.up_to_30),
-            backgroundColor: 'orange'
-          },
-          {
-            label: 'More than 60 days',
-            data: data_prs_days_open_2021.map(row => row.over_60),
-            backgroundColor: 'red'
-          }
-        ]
-      },
-    }
+    config_2021
   );
 
-  // PRs closed in 2020
+  // PRs 2020: configure and create
+  const data_2020 = {
+    labels: data_prs_days_open_2020.map(row => row.woy),
+    datasets: [
+      {
+        label: 'Up to 10 days',
+        data: data_prs_days_open_2020.map(row => row.up_to_10),
+        backgroundColor: '#66b237'
+      },
+      {
+        label: 'Up to 30 days',
+        data: data_prs_days_open_2020.map(row => row.up_to_30),
+        backgroundColor: 'orange'
+      },
+      {
+        label: 'More than 60 days',
+        data: data_prs_days_open_2020.map(row => row.over_60),
+        backgroundColor: 'red'
+      }
+    ]
+  };
+
+  // clone plugin config and customize title
+  const plugins_2020 = structuredClone(plugins);
+  plugins_2020.title.text='[2020] Contrast PRs and bugfixes'
+  const config_2020 = {
+    type: 'line',
+    data: data_2020,
+    options: {
+      scales: scales,
+      plugins: plugins_2020,
+      elements: elements,
+      onClick(e) {
+        const chart = e.chart;
+        console.debug('Clicked: ' + chart.options.plugins.title.text)
+        chart.options.plugins.zoom.zoom.wheel.enabled = !chart.options.plugins.zoom.zoom.wheel.enabled;
+        chart.options.plugins.zoom.zoom.pinch.enabled = !chart.options.plugins.zoom.zoom.pinch.enabled;
+        chart.update();
+      },
+    },
+  };
+
   const chart_2020 = new Chart(
     ctx_prs_days_open_2020,
-    {
-      type: 'line',
-      options: opts_2020,
-      data: {
-        labels: data_prs_days_open_2020.map(row => row.woy),
-        datasets: [
-          {
-            label: 'Up to 10 days',
-            data: data_prs_days_open_2020.map(row => row.up_to_10),
-            backgroundColor: '#66b237'
-          },
-          {
-            label: 'Up to 30 days',
-            data: data_prs_days_open_2020.map(row => row.up_to_30),
-            backgroundColor: 'orange'
-          },
-          {
-            label: 'More than 60 days',
-            data: data_prs_days_open_2020.map(row => row.over_60),
-            backgroundColor: 'red'
-          }
-        ]
-      },
-    }
+    config_2020
   );
 
-  resetZoomBtn2023 = () => {
+  const resetZoomBtn2023 = () => {
     chart_2023.resetZoom()
   };
 
-  resetZoomBtn2022 = () => {
+  const resetZoomBtn2022 = () => {
     chart_2022.resetZoom()
   };
 
-  resetZoomBtn2021 = () => {
+  const resetZoomBtn2021 = () => {
     chart_2021.resetZoom()
   };
 
-  resetZoomBtn2020 = () => {
+  const resetZoomBtn2020 = () => {
     chart_2020.resetZoom()
   };
 
